0f895a0da76e6ae2460ac867e7fddbdd73228298
MrNilz <mrnilz@MrNilzs-MacBook-Air.local>
2022-10-21 13:36:37 +0200

    try to improve scapy performance through scapy config

--- var/wake_host.py
+++ var/wake_host.py
@@ -2,7 +2,7 @@
 
 import syslog
 import sys
-from scapy.all import sniff,DNS
+from scapy.all import sniff,conf,DNS,IP,IPv6,UDP
 from wakeonlan import send_magic_packet
 
 
@@ -14,8 +14,6 @@ listen_ip   = sys.argv[2]
 listen_dns = sys.argv[3].encode('utf-8')
 host_ip  = sys.argv[4]
 host_mac = sys.argv[5]
- 
-syslog.syslog(f'Watching for DNS Query {listen_dns} on Interface {listen_interface} IP {listen_ip} and wake up IP {host_ip} MAC {host_mac} if DNS Query occurs.')
 
 
 def handler(req):
@@ -25,4 +23,12 @@ def handler(req):
         syslog.syslog(f"Received DNS query for {listen_dns} and send magic package to IP: {host_ip} MAC: {host_mac}")
 
 if __name__ == '__main__':
+
+    syslog.syslog(f'Start watching for DNS Query {listen_dns} on Interface {listen_interface} IP {listen_ip} and wake up IP {host_ip} MAC {host_mac} if DNS Query occurs.')
+    
+    conf.promisc = False
+    conf.layers.filter([IP, IPv6, UDP, DNS])
+
     sniff(iface=f"{listen_interface}",filter=f"ip dst {listen_ip} and udp and port 53", lfilter=lambda x: x.haslayer(DNS), prn=handler, store=0)
+
+    syslog.syslog(f'Stopped watching for DNS Query {listen_dns}.')
